1a1b4f9bd994be78dd457c9fe024b7d484a9b17b
Vasiliy Bolgar <reanimatorzon@users.noreply.github.com>
2020-08-17 23:28:24 +0300

    Add .gitignore & import.sh

--- import.sh
+++ import.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
+# TODO: wget as an alternative
 # shellcheck disable=SC1090
-.import() { if [ "${1:0:4}" = "http" ]; then source <(curl -s "$1"); else source "$1"; fi; }
-
+.import() { if [ "${1:0:4}" = 'http' ]; then .<(curl -s "$1"); else source "$1"; fi; }
