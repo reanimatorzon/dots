b6eaf2ab191750ac6d6525a8ef49d21db9cbceec
Vasiliy Bolgar <reanimatorzon@users.noreply.github.com>
2020-08-18 15:51:35 +0300

    Add examples

--- example.sh
+++ example.sh
@@ -4,7 +4,8 @@
 # You can use `source <(curl -s $URL)` and `source $FILENAME` instead
 source <(curl -s https://raw.githubusercontent.com/reanimatorzon/dots/master/import.sh)
 
-.import ./plugins/demoer.sh
+#.import ./plugins/demoer.sh
+.import https://raw.githubusercontent.com/reanimatorzon/dots/master/plugins/demoer.sh
 
 # test call
 .demoer PARAM1
